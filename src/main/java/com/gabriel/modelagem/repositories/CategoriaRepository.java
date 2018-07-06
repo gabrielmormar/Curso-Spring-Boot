658cb0b2596ed78981b4a5e0b2ea1c7c39bc09eb
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-07-06 20:23:35 -0300

    Criado Repository e Service para Categoria

---
+++ src/main/java/com/gabriel/modelagem/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.gabriel.modelagem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabriel.modelagem.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}
