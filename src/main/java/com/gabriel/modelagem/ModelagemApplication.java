a0092df16bd9be26241d07bd3768b5e260a2a258
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-07-06 20:41:10 -0300

    Criado instanciacao

--- src/main/java/com/gabriel/modelagem/ModelagemApplication.java
+++ src/main/java/com/gabriel/modelagem/ModelagemApplication.java
@@ -1,12 +1,30 @@
 package com.gabriel.modelagem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelagemApplication {
+import com.gabriel.modelagem.domain.Categoria;
+import com.gabriel.modelagem.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ModelagemApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
