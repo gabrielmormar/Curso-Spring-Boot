7f7067711d565373da3dc658700a1954ae5d13d5
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-07-07 17:11:39 -0300

    Estado e Cidade

--- src/main/java/com/gabriel/modelagem/ModelagemApplication.java
+++ src/main/java/com/gabriel/modelagem/ModelagemApplication.java
@@ -8,18 +8,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gabriel.modelagem.domain.Categoria;
+import com.gabriel.modelagem.domain.Cidade;
+import com.gabriel.modelagem.domain.Estado;
 import com.gabriel.modelagem.domain.Produto;
 import com.gabriel.modelagem.repositories.CategoriaRepository;
+import com.gabriel.modelagem.repositories.CidadeRepository;
+import com.gabriel.modelagem.repositories.EstadoRepository;
 import com.gabriel.modelagem.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ModelagemApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoriaRepository categoriaRepository;
+	private CategoriaRepository categoriaRepository;	
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;	
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
@@ -39,9 +49,23 @@ public class ModelagemApplication implements CommandLineRunner {
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat1));		
 						
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Bahia");
+		Estado est2 = new Estado(null, "Minas Gerais");
+		
+		Cidade cid1 = new Cidade(null, "Salvador", est1);
+		Cidade cid2 = new Cidade(null, "Uberaba", est2);
+		Cidade cid3 = new Cidade(null, "Patos de Minas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
+		
 	}
 }
---
+++ src/main/java/com/gabriel/modelagem/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.gabriel.modelagem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabriel.modelagem.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	
+}
+
