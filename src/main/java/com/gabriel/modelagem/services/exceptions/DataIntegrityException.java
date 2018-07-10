10bf4c303de7f943b3e6e13cf8dc230ba3e456ca
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-07-09 22:36:41 -0300

    Deletando uma Categoria com DELETE

---
+++ src/main/java/com/gabriel/modelagem/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,14 @@
+package com.gabriel.modelagem.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	
+	public DataIntegrityException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
