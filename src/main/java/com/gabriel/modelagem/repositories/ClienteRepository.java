6cf504789f1e96228d0753d868f9a67f28e7ddce
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-07-14 00:00:20 -0300

    Busca de Produto por Nome e Categoria

--- src/main/java/com/gabriel/modelagem/repositories/ClienteRepository.java
+++ src/main/java/com/gabriel/modelagem/repositories/ClienteRepository.java
@@ -1,16 +1,15 @@
 package com.gabriel.modelagem.repositories;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gabriel.modelagem.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	Cliente findByEmail(String email);
 }
 
