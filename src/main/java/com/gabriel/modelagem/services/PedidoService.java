c6045aed8bb6a64ba71d825912b49d65535f9b4a
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-07-14 20:52:07 -0300

    Inserindo Pedido

--- src/main/java/com/gabriel/modelagem/services/PedidoService.java
+++ src/main/java/com/gabriel/modelagem/services/PedidoService.java
@@ -1,11 +1,17 @@
 package com.gabriel.modelagem.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gabriel.modelagem.domain.ItemPedido;
+import com.gabriel.modelagem.domain.PagamentoComBoleto;
 import com.gabriel.modelagem.domain.Pedido;
+import com.gabriel.modelagem.domain.enums.EstadoPagamento;
+import com.gabriel.modelagem.repositories.ItemPedidoRepository;
+import com.gabriel.modelagem.repositories.PagamentoRepository;
 import com.gabriel.modelagem.repositories.PedidoRepository;
 import com.gabriel.modelagem.services.exceptions.ObjectNotFoundException;
 
@@ -15,10 +21,44 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		
 		Optional<Pedido> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagamento = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagamento, obj.getInstante());
+		}
+		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido itemPedido : obj.getItens()) {
+			itemPedido.setDesconto(0.0);
+			itemPedido.setPreco(produtoService.find(itemPedido.getProduto().getId()).getPreco());
+			itemPedido.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
