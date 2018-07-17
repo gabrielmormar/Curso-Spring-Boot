4f977f578e5f9e2b765cd0c415e16daef817adfe
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-07-17 20:33:52 -0300

    Criado profile de Desenvolvimento

---
+++ src/main/java/com/gabriel/modelagem/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.gabriel.modelagem.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.gabriel.modelagem.services.DBservice;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBservice dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.intantiateTestDatabase();
+		return true;
+	}
+}
