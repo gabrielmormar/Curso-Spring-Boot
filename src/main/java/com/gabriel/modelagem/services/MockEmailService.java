8f45111ee8e1af37a99e760a64ab5454a02cf819
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-07-18 10:54:28 -0300

    Email HTML

--- src/main/java/com/gabriel/modelagem/services/MockEmailService.java
+++ src/main/java/com/gabriel/modelagem/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.gabriel.modelagem.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -9,9 +11,16 @@ public class MockEmailService extends AbstractEmailService{
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
 	
 	@Override
-	public void sendEmail(SimpleMailMessage msg) {
-		
+	public void sendEmail(SimpleMailMessage msg) {		
 		LOG.info("Simulando o envio de email");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando o envio de email HTML...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 		
