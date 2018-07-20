3103c177df94fe0a05c30d4fa96d1ec25ca1d4e6
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-07-19 21:02:56 -0300

    Implementado a Autorização

--- src/main/java/com/gabriel/modelagem/config/SecurityConfig.java
+++ src/main/java/com/gabriel/modelagem/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.gabriel.modelagem.security.JWTAuthenticationFilter;
+import com.gabriel.modelagem.security.JWTAuthorizationFilter;
 import com.gabriel.modelagem.security.JWTUtil;
 
 
@@ -53,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
